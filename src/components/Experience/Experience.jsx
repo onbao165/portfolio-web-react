987ebecfcf788258ee4fb40ce104b4af52fa233a
onbao165 <ongiabaoit22@gmail.com>
2023-07-04 17:09:37 +0700

    experience html

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import './experience.css'
-
+import React from "react";
+import "./experience.css";
+import { BiCube } from "react-icons/bi";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5></h5>
+      <h2>My Experience</h2>
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          {/* Frontend */}
+          <h3>FrontEnd Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BiCube />
+              <h4>HTML</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience_details">
+              <BiCube />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience_details">
+              <BiCube />
+              <h4>JAVASCRIPT</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience_details">
+              <BiCube />
+              <h4>REACT</h4>
+              <small className="text-light">Beginner</small>
+            </article>
+          </div>
+        </div>
+        {/* Backend */}
+        <div className="experience-backend">
+          <h3>BackEnd Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BiCube />
+              <h4>JAVA SPRING BOOT</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience_details">
+              <BiCube />
+              <h4>DOCKER</h4>
+              <small className="text-light">Beginner</small>
+            </article>
+            <article className="experience_details">
+              <BiCube />
+              <h4>NODEJS</h4>
+              <small className="text-light">Beginner</small>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
