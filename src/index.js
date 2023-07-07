815609d772c9cca7852934f0a79721d205fe11ad
barooon165 <112069172+onbao165@users.noreply.github.com>
2023-07-07 20:11:05 +0700

    switch to using createRoot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import './assets/style.css'
-ReactDOM.render(<App/>, document.querySelector("#root")) //for rendering the web
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(<App/>); 
+
+// ReactDOM.createRoot is used to create a root-level entry point for your React application, 
+// and the render method is called to render your App component into the specified root element.
